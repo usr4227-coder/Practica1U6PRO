6c6ad7a729c96ef741332a587202d0a431875362
Nick <usr4227@salesianos-lacuesta.net>
2026-04-30 17:30:24 +0100

    feat: Creacion de atributos y constructores de Asistente

---
+++ Ejercicio2/src/net/salesianos/asistente/Asistente.java
@@ -0,0 +1,13 @@
+package net.salesianos.asistente;
+
+public class Asistente {
+    private String dni;
+    private String nombre;
+    private String grupo;
+
+    public Asistente(String dni, String nombre, String grupo) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.grupo = grupo;
+    }
+}
