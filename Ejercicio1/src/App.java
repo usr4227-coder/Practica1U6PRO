e9e8d2fbc8ed41b6a359000ee3f8f257cc9b7602
Nick <usr4227@salesianos-lacuesta.net>
2026-04-30 13:52:00 +0100

    feat: Pool y Push

--- Ejercicio1/src/App.java
+++ Ejercicio1/src/App.java
@@ -16,5 +16,23 @@ public class App {
 
         System.out.println("Reparaciones pendientes:");
         for (Reparacion r : cola) System.out.println(r);
+         System.out.println("\nPrimera reparación sin eliminar:");
+        System.out.println(cola.peek());
+
+        Reparacion r1 = cola.poll();
+        Reparacion r2 = cola.poll();
+
+        pila.push(r1);
+        pila.push(r2);
+
+        System.out.println("\nÚltima reparación atendida:");
+        System.out.println(pila.peek());
+
+        System.out.println("\nExtrayendo una del historial:");
+        System.out.println(pila.pop());
+
+        System.out.println("\nReparaciones pendientes: " + cola.size());
+        System.out.println("¿Cola vacía? " + cola.isEmpty());
+    
     }
 }
