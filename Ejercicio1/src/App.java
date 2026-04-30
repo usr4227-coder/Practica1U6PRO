1423bb50bf5377457edd882b0296eff7197c4ca7
Nick <usr4227@salesianos-lacuesta.net>
2026-04-30 13:48:07 +0100

    feat: AppCreaciónCola

--- Ejercicio1/src/App.java
+++ Ejercicio1/src/App.java
@@ -1,5 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import net.salesianos.reparacion.Reparacion;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Queue<Reparacion> cola = new LinkedList<>();
+        Stack<Reparacion> pila = new Stack<>();
+
+        cola.add(new Reparacion("R001", "Ana", "Cambio de pantalla", false));
+        cola.add(new Reparacion("R002", "Luis", "Batería dañada", true));
+        cola.add(new Reparacion("R003", "Marta", "Teclado roto", false));
+        cola.add(new Reparacion("R004", "Pedro", "Puerto de carga", true));
+
+        System.out.println("Reparaciones pendientes:");
+        for (Reparacion r : cola) System.out.println(r);
     }
 }
