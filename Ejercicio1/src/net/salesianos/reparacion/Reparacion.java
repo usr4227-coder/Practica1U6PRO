4864fd12a1c440c82a6a6f0d63a6fc266259106e
Nick <usr4227@salesianos-lacuesta.net>
2026-04-30 13:41:39 +0100

    feat: Creacion de Getters y Setters de Reparacion

--- Ejercicio1/src/net/salesianos/reparacion/Reparacion.java
+++ Ejercicio1/src/net/salesianos/reparacion/Reparacion.java
@@ -12,4 +12,15 @@ public class Reparacion {
         this.descripcion = descripcion;
         this.urgente = urgente;
     }
+
+    public String getCodigo() { return codigo; }
+    public String getCliente() { return cliente; }
+    public String getDescripcion() { return descripcion; }
+    public boolean isUrgente() { return urgente; }
+
+    public void setCodigo(String codigo) { this.codigo = codigo; }
+    public void setCliente(String cliente) { this.cliente = cliente; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public void setUrgente(boolean urgente) { this.urgente = urgente; }
+
 }
